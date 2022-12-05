d9961d6af070101ed52befab757ab26de78a1b4e
Elmigdad Sirry <migdad.sirry@gmail.com>
2022-12-05 16:36:57 +0400

    Update Program.cs
    add comment

--- Program.cs
+++ Program.cs
@@ -20,6 +20,19 @@ class Result
      *
      * The function is expected to return an INTEGER_ARRAY.
      * The function accepts INTEGER_ARRAY scores as parameter.
+     
+     Example
+scores=[12,24,20,24]
+Scores are in the same order as the games played. She tabulates her results as follows:
+
+                                     Count
+    Game  Score  Minimum  Maximum   Min Max
+     0      12     12       12       0   0
+     1      24     12       24       0   1
+     2      10     10       24       1   1
+     3      24     10       24       1   1
+Given the scores for a season, determine the number of times Maria breaks her records for most and least points scored during the season.
+     
      */
 
     public static List<int> breakingRecords(List<int> scores)
